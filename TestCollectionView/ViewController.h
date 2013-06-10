bce71a1c2398373fdcff2e02f6ccde4a1f5b9998
Forrest <apple.dev.sh@gmail.com>
2013-06-10 10:51:20 +0800

    Initial Commit

---
+++ TestCollectionView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestCollectionView
+//
+//  Created by forrest on 13-6-10.
+//  Copyright (c) 2013年 DFA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TestCollectionView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestCollectionView
+//
+//  Created by forrest on 13-6-10.
+//  Copyright (c) 2013年 DFA. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
